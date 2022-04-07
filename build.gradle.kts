b77c4afae6921c63bb60fe58fa0d1ab401e83e8a
Behnam <safari.b1987@gmail.com>
2022-04-07 12:20:56 +0200

    prepare dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.palantir.docker") version "0.22.1"
 	war
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
@@ -10,6 +11,7 @@ plugins {
 
 group = "com.bootcamp.labs"
 version = "0.0.1-SNAPSHOT"
+description = "Hotel Gift Card - lab 1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -22,6 +24,14 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation ("org.axonframework:axon-spring-boot-starter:4.5.9")
+	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly ("com.h2database:h2")
+	testImplementation ("org.axonframework:axon-test:4.5.9")
+	testImplementation ("org.junit.jupiter:junit-jupiter-api")
+	testImplementation ("org.junit.jupiter:junit-jupiter-params")
+	testImplementation ("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.withType<KotlinCompile> {
